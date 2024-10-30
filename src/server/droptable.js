64af22277dd381d51fac41b7fb37ebeb6477c099
gulis007 <luisgfinger@gmail.com>
2024-10-29 22:12:20 -0300

    aplicando sql lite

---
+++ src/server/droptable.js
@@ -0,0 +1,18 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./database.db', (err) => {
+    if (err) {
+      console.error("Erro ao conectar ao banco de dados:", err);
+    } else {
+      console.log("Conectado ao banco de dados SQLite.");
+    }
+  });
+
+db.run("DROP TABLE IF EXISTS pedido", (err) => {
+    if (err) {
+      console.error("Erro ao excluir tabela:", err);
+    } else {
+      console.log("Tabela 'fornecedor' excluída com sucesso.");
+    }
+  });
+  
---
+++ src/server/index.js
@@ -0,0 +1,394 @@
+const express = require("express");
+const multer = require("multer");
+const sqlite3 = require("sqlite3").verbose();
+const fs = require("fs");
+const app = express();
+const cors = require("cors");
+const port = 3001;
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/uploads', express.static('uploads'));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post("/upload", upload.single("imagem"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: "Nenhum arquivo enviado" });
+  }
+  const imageUrl = `/uploads/${req.file.filename}`;
+  res.json({ imageUrl });
+});
+
+
+const db = new sqlite3.Database("./database.db", (err) => {
+  if (err) {
+    console.error("Erro ao conectar ao banco de dados:", err);
+  } else {
+    db.run("PRAGMA foreign_keys = ON;");
+    console.log("Conectado ao banco de dados SQLite.");
+  }
+});
+
+
+db.run(` 
+  CREATE TABLE IF NOT EXISTS fornecedor (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    cnpj TEXT NOT NULL,
+    contato TEXT NOT NULL,
+    endereco TEXT NOT NULL
+  );`);
+
+db.run(`  
+  CREATE TABLE IF NOT EXISTS cliente(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  nome TEXT NOT NULL,
+  cpf_cnpj TEXT NOT NULL,
+  contato TEXT NOT NULL,
+  endereco TEXT NOT NULL
+  );`);
+
+db.run(`
+  CREATE TABLE IF NOT EXISTS produto(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    descricao TEXT NOT NULL,
+    preco FLOAT NOT NULL,
+    quantidade INTEGER NOT NULL,
+    imagem TEXT NOT NULL,
+    fornecedorId INTEGER,
+    FOREIGN KEY(fornecedorId) REFERENCES fornecedor(id)
+  );
+`);
+
+db.run(`
+  CREATE TABLE IF NOT EXISTS pedido(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  data DATE,
+  fornecedorId INTEGER,
+  status TEXT,
+  total FLOAT,
+  FOREIGN KEY(fornecedorId) REFERENCES fornecedor(id)
+  );
+  `);
+
+  db.run(`
+    CREATE TABLE IF NOT EXISTS itemPedido(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    pedidoId INTEGER,
+    produtoId INTEGER,
+    quantidade INTEGER,
+    precoUnitario FLOAT,
+    FOREIGN KEY (pedidoId) REFERENCES pedido(id),
+    FOREIGN KEY (produtoId) REFERENCES produto(id)
+    );
+    `);
+
+app.get("/api/fornecedor", (req, res) => {
+  db.all("SELECT * FROM fornecedor", [], (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json(rows);
+  });
+});
+
+app.post("/api/fornecedor", (req, res) => {
+  const { nome, cnpj, contato, endereco } = req.body;
+  db.run(
+    "INSERT INTO fornecedor (nome, cnpj, contato, endereco) VALUES (?, ?, ?, ?)",
+    [nome, cnpj, contato, endereco],
+    function (err) {
+      if (err) {
+        console.error("Erro ao inserir fornecedor:", err);
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({ id: this.lastID });
+    }
+  );
+});
+
+app.put("/api/fornecedor/:id", (req, res) => {
+  const { id } = req.params;
+  const { nome, cnpj, contato, endereco } = req.body;
+  db.run(
+    "UPDATE fornecedor SET nome = ?, cnpj = ?, contato = ?, endereco = ? WHERE id = ?",
+    [nome, cnpj, contato, endereco, id],
+    function (err) {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({ updatedRows: this.changes });
+    }
+  );
+});
+
+app.delete("/api/fornecedor/:id", (req, res) => {
+  const { id } = req.params;
+  db.run("DELETE FROM fornecedor WHERE id = ?", id, function (err) {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({ deletedRows: this.changes });
+  });
+});
+
+
+app.get("/api/cliente", (req, res) => {
+  db.all("SELECT * FROM cliente", [], (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json(rows);
+  });
+});
+
+app.post("/api/cliente", (req, res) => {
+  const { nome, cpf_cnpj, contato, endereco } = req.body;
+  db.run(
+    "INSERT INTO cliente (nome, cpf_cnpj, contato, endereco) VALUES (?, ?, ?, ?)",
+    [nome, cpf_cnpj, contato, endereco],
+    function (err) {
+      if (err) {
+        console.error("Erro ao inserir cliente:", err);
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({ id: this.lastID });
+    }
+  );
+});
+
+app.put("/api/cliente/:id", (req, res) => {
+  const { id } = req.params;
+  const { nome, cpf_cnpj, contato, endereco } = req.body;
+  db.run(
+    "UPDATE cliente SET nome = ?, cpf_cnpj = ?, contato = ?, endereco = ? WHERE id = ?",
+    [nome, cpf_cnpj, contato, endereco, id],
+    function (err) {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({ updatedRows: this.changes });
+    }
+  );
+});
+
+app.delete("/api/cliente/:id", (req, res) => {
+  const { id } = req.params;
+  db.run("DELETE FROM cliente WHERE id = ?", id, function (err) {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({ deletedRows: this.changes });
+  });
+});
+
+
+app.get("/api/produto", (req, res) => {
+  db.all("SELECT * FROM produto", [], (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json(rows);
+  });
+});
+
+app.post("/api/produto", (req, res) => {
+  const { nome, descricao, preco, quantidade, imagem, fornecedorId } = req.body;
+  db.run(
+    "INSERT INTO produto (nome, descricao, preco, quantidade, imagem, fornecedorId) VALUES (?, ?, ?, ?, ?, ?)",
+    [nome, descricao, preco, quantidade, imagem, fornecedorId],
+    function (err) {
+      if (err) {
+        console.error("Erro ao inserir produto:", err);
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({ id: this.lastID });
+    }
+  );
+});
+
+app.put("/api/produto/:id", (req, res) => {
+  const { id } = req.params;
+  const { nome, descricao, preco, quantidade, imagem, fornecedorId } = req.body;
+  db.run(
+    "UPDATE produto SET nome = ?, descricao = ?, preco = ?, quantidade = ?, imagem = ?, fornecedorId = ? WHERE id = ?",
+    [nome, descricao, preco, quantidade, imagem, fornecedorId, id],
+    function (err) {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({ updatedRows: this.changes });
+    }
+  );
+});
+
+
+app.delete("/api/produto/:id", (req, res) => {
+  const { id } = req.params;
+
+
+  db.get("SELECT imagem FROM produto WHERE id = ?", id, (err, row) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+
+    if (!row) {
+      res.status(404).json({ error: "Produto não encontrado" });
+      return;
+    }
+
+
+    const imagePath = `.${row.imagem}`;
+
+ 
+    db.run("DELETE FROM produto WHERE id = ?", id, function (err) {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+
+      fs.unlink(imagePath, (unlinkErr) => {
+        if (unlinkErr) {
+          console.error("Erro ao deletar a imagem:", unlinkErr);
+        }
+        res.json({ deletedRows: this.changes });
+      });
+    });
+  });
+});
+
+app.get("/api/pedido", (req, res) => {
+  db.all("SELECT * FROM pedido", [], (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json(rows);
+  });
+});
+
+app.post("/api/pedido", (req, res) => {
+  const { data, fornecedorId, status, total } = req.body;
+  db.run(
+    "INSERT INTO pedido (data, fornecedorId, status, total) VALUES (?, ?, ?, ?)",
+    [data, fornecedorId, status, total],
+    function (err) {
+      if (err) {
+        console.error("Erro ao inserir pedido:", err);
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({ id: this.lastID });
+    }
+  );
+});
+
+app.put("/api/pedido/:id", (req, res) => {
+  const { id } = req.params;
+  const { data, fornecedorId, status, total } = req.body;
+  db.run(
+    "UPDATE pedido SET data = ?, fornecedorId = ?, status = ?, total = ? WHERE id = ?",
+    [data, fornecedorId, status, total, id],
+    function (err) {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({ updatedRows: this.changes });
+    }
+  );
+});
+
+app.delete("/api/pedido/:id", (req, res) => {
+  const { id } = req.params;
+  db.run("DELETE FROM pedido WHERE id = ?", id, function (err) {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({ deletedRows: this.changes });
+  });
+});
+
+app.get("/api/itempedido", (req, res) => {
+  db.all("SELECT * FROM itemPedido", [], (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json(rows);
+  });
+});
+
+app.post("/api/itempedido", (req, res) => {
+  const { pedidoId, produtoId, quantidade, precoUnitario } = req.body;
+  db.run(
+    "INSERT INTO itemPedido (pedidoId, produtoId, quantidade, precoUnitario) VALUES (?, ?, ?, ?)",
+    [pedidoId, produtoId, quantidade, precoUnitario],
+    function (err) {
+      if (err) {
+        console.error("Erro ao inserir itemPedido:", err);
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({ id: this.lastID });
+    }
+  );
+});
+
+app.put("/api/itempedido/:id", (req, res) => {
+  const { id } = req.params;
+  const { pedidoId, produtoId, quantidade, precoUnitario } = req.body;
+  db.run(
+    "UPDATE itemPedido SET pedidoId = ?, produtoId = ?, quantidade = ?, precoUnitario = ? WHERE id = ?",
+    [pedidoId, produtoId, quantidade, precoUnitario, id],
+    function (err) {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({ updatedRows: this.changes });
+    }
+  );
+});
+
+app.delete("/api/itempedido/:id", (req, res) => {
+  const { id } = req.params;
+  db.run("DELETE FROM itemPedido WHERE id = ?", id, function (err) {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({ deletedRows: this.changes });
+  });
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
